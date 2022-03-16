8832ff409f4e2ae0202376dfab92a1573aacfdd6
NULL511 <null511@outlook.com>
2022-03-16 15:20:26 -0400

    finish goat model

--- MinecraftMappings.NET/Minecraft/Java/Models/Entity/Goat.cs
+++ MinecraftMappings.NET/Minecraft/Java/Models/Entity/Goat.cs
@@ -41,73 +41,106 @@ namespace MinecraftMappings.Minecraft.Java.Models.Entity
                         },
                     });
                 })
-                .AddElement("body", element => {
-                    element.Part = "body";
-                    element.Translate = new Vector3(0f, -19f, -2f);
+                .AddElement("nose", element => {
+                    element.Part = "nose";
+                    element.Translate = new Vector3(0.5f, -18f, 8f);
+                    element.RotationAngleX = -55;
                     element.InvertAxisX = true;
                     element.InvertAxisY = true;
 
-                    element.Submodels.Add(new EntityElement("rotation") {
-                        Translate = new Vector3(0f, 19f, 2f),
-                        RotationAngleX = -90f,
-                        InvertAxisX = true,
-                        InvertAxisY = true,
+                    element.Boxes.Add(new EntityElementCube {
+                        Position = new Vector3(-2.5f, 15f, -16f),
+                        Size = new Vector3(5f, 7f, 10f),
+                        UV = new Vector2(34f, 46f),
+                    });
+                })
+                .AddElement("left_horn", element => {
+                    element.Part = "left_horn";
+                    element.Translate = new Vector3(0.5f, -10f, 0f);
+                    element.InvertAxisX = true;
+                    element.InvertAxisY = true;
 
-                        Boxes = {
-                            new EntityElementCube {
-                                Position = new Vector3(-4f, -6f, -7f),
-                                Size = new Vector3(8f, 16f, 6f),
-                                UV = new Vector2(28f, 8f),
-                            },
-                        },
+                    element.Boxes.Add(new EntityElementCube {
+                        Position = new Vector3(-2.49f, 19f, -10f),
+                        Size = new Vector3(2f, 7f, 2f),
+                        UV = new Vector2(12f, 55f),
+                    });
+                })
+                .AddElement("right_horn", element => {
+                    element.Part = "right_horn";
+                    element.Translate = new Vector3(0.5f, -10f, 0f);
+                    element.InvertAxisX = true;
+                    element.InvertAxisY = true;
+
+                    element.Boxes.Add(new EntityElementCube {
+                        Position = new Vector3(0.49f, 19f, -10f),
+                        Size = new Vector3(2f, 7f, 2f),
+                        UV = new Vector2(12f, 55f),
+                    });
+                })
+                .AddElement("body", element => {
+                    element.Part = "body";
+                    element.Translate = new Vector3(-0.5f, 0f, 0f);
+                    element.InvertAxisX = true;
+                    element.InvertAxisY = true;
+
+                    element.Boxes.Add(new EntityElementCube {
+                        Position = new Vector3(-4.5f, 6f, -7f),
+                        Size = new Vector3(9f, 11f, 16f),
+                        UV = new Vector2(1f, 1f),
+                    });
+                    element.Boxes.Add(new EntityElementCube {
+                        Position = new Vector3(-5.5f, 4f, -8f),
+                        Size = new Vector3(11f, 14f, 11f),
+                        UV = new Vector2(0f, 28f),
                     });
                 })
                 .AddElement("leg1", element => {
                     element.Part = "leg1";
-                    element.Translate = new Vector3(-3f, -12f, -7f);
+                    element.Translate = new Vector3(-3.5f, -10f, -4f);
                     element.InvertAxisX = true;
                     element.InvertAxisY = true;
 
                     element.Boxes.Add(new EntityElementCube {
-                        Position = new Vector3(1f, 0f, 5f),
-                        Size = new Vector3(4f, 12f, 4f),
-                        UV = new Vector2(0f, 16f),
+                        Position = new Vector3(0.5f, 0f, 4f),
+                        Size = new Vector3(3f, 6f, 3f),
+                        UV = new Vector2(49f, 29f),
                     });
                 })
                 .AddElement("leg2", element => {
                     element.Part = "leg2";
-                    element.Translate = new Vector3(3f, -12f, -7f);
+                    element.Translate = new Vector3(0.5f, -10f, -4f);
                     element.InvertAxisX = true;
                     element.InvertAxisY = true;
 
                     element.Boxes.Add(new EntityElementCube {
-                        Position = new Vector3(-5f, 0f, 5f),
-                        Size = new Vector3(4f, 12f, 4f),
-                        UV = new Vector2(0f, 16f),
+                        Position = new Vector3(-3.5f, 0f, 4f),
+                        Size = new Vector3(3f, 6f, 3f),
+                        UV = new Vector2(36f, 29f),
                     });
                 })
                 .AddElement("leg3", element => {
                     element.Part = "leg3";
-                    element.Translate = new Vector3(-3f, -12f, 5f);
+                    element.Translate = new Vector3(-3.5f, -10f, 6f);
                     element.InvertAxisX = true;
                     element.InvertAxisY = true;
 
                     element.Boxes.Add(new EntityElementCube {
-                        Position = new Vector3(1f, 0f, -7f),
-                        Size = new Vector3(4f, 12f, 4f),
-                        UV = new Vector2(0f, 16f),
+                        Position = new Vector3(0.5f, 0f, -6f),
+                        Size = new Vector3(3f, 10f, 3f),
+                        UV = new Vector2(49f, 2f),
                     });
                 })
                 .AddElement("leg4", element => {
                     element.Part = "leg4";
-                    element.Translate = new Vector3(3f, -12f, 5f);
+                    element.Translate = new Vector3(0.5f, -10f, 6f);
                     element.InvertAxisX = true;
                     element.InvertAxisY = true;
 
                     element.Boxes.Add(new EntityElementCube {
-                        Position = new Vector3(-5f, 0f, -7f),
-                        Size = new Vector3(4f, 12f, 4f),
-                        UV = new Vector2(0f, 16f),
+                        Position = new Vector3(-3.5f, 0f, -6f),
+                        Size = new Vector3(3f, 10f, 3f),
+                        UV = new Vector2(35f, 2f),
                     });
                 });
         }
